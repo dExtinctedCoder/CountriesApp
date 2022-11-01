579517b43f2620b52ff07055104b8f9a842020d8
Resistor057 <salaudeenmuhdjamiu1@gmail.com>
2022-11-01 21:29:22 +0100

    Update Error page jsx

--- src/components/Reusables/Error.jsx
+++ src/components/Reusables/Error.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className=" px-8 pt-16 py-5">
+      <h3 className=" text-center mb-10 text-4xl font-bold dark:text-textDark">Page Not Found!</h3>
+      <Link to="/" className=" text-sm ml-3 py-2 px-4 text-textDark bg-bgDark dark:bg-inputLight">Back to home</Link>
+    </div>
   )
 }
 
